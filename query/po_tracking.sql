2611e1737f1859ee565cb861f30d52663712ae30
sandeep vishwakarma <sandeepvishwakarma9@gmail.com>
2026-05-19 13:44:28 +0530

    Update po_tracking.sql

--- query/po_tracking.sql
+++ query/po_tracking.sql
@@ -1,1 +1,19 @@
 
+/*
+Project : SAP B1 PO Tracking Query
+Author  : Sandeep Vishwakarma
+Purpose : Track PO to Payment lifecycle
+
+Database : SAP HANA
+*/
+--------------------------------------------------
+-- PURCHASE ORDER DETAILS
+--------------------------------------------------
+/* SELECT FROM POR1 T1 */
+
+DECLARE ItemCode NVARCHAR(50);
+
+ItemCode := /* T1."ItemCode" */ '[%0]';
+
+SELECT
+
